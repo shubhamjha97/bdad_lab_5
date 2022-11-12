e436d07b83d7d94f5fc020678d2ddbe5496a70bd
Shubham Jha <jha1shubham@gmail.com>
2022-11-11 21:55:26 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+## Lab 5 (Shubham Jha, sj3549)
+
+### Usage
+You can compile code with `sbt compile`, run it with `sbt run`, and `sbt console` will start a Scala 3 REPL.
---
+++ src/main/scala/Main.scala
@@ -0,0 +1,34 @@
+
+def loadData(path: String): org.apache.spark.rdd.RDD[(String, String)] = {
+  sc.wholeTextFiles(path)
+}
+
+def flattenData(data: org.apache.spark.rdd.RDD[(String, String)]): org.apache.spark.rdd.RDD[String] = {
+  data.map(x => x._2)
+}
+
+def parseData(data: org.apache.spark.rdd.RDD[String]): org.apache.spark.rdd.RDD[(String, String)] = {
+  data.map(scala.xml.XML.loadString).map(x => x \ "activation").flatMap(x=>x).map(x => (x \ "account-number", x \ "model")).map(x=>(x._1.text, x._2.text))
+}
+
+def formatData(data: org.apache.spark.rdd.RDD[(String, String)]): org.apache.spark.rdd.RDD[String] = {
+  data.map(x => s"${x._1}:${x._2}")
+}
+
+@main
+def main: Unit = {
+  // Load data
+  val rawData = loadData("loudacre/activations")
+
+  // Flatten data
+  val flatData = flattenData(rawData)
+
+  // Parse XML
+  val parsedData = parseData(flatData)
+
+  // Output Formatting
+  val output = formatData(parsedData)
+
+  // Write output to file
+  output.saveAsTextFile("loudacre/account-models")
+}
